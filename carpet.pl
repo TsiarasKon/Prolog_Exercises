85c46a980bd313acbb7d582970b5f25310d85ca9
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2018-03-21 12:36:42 +0200

    Comments

--- carpet.pl
+++ carpet.pl
@@ -1,39 +1,46 @@
 carpet(N) :-
 	N1 is N - 1,
-	rewrite(_, Carpet),
+	rewrite(_, Carpet),	% carpet_help/2 is getting the list of lists of the first rewrite/2 found
 	carpet_help(Carpet, N1), !.
 	
+/* While N > 0, apply a recarpet/2 and recursively call itself until N == 0.
+ * After every call we're always left with a "2D matrix" (list of lists). */
 carpet_help(Carpet, 0) :- carpet_print(Carpet).
 carpet_help(Carpet, N) :-
 	N1 is N - 1,
 	recarpet(Carpet, NewCarpet),
 	carpet_help(NewCarpet, N1).
 
+/* Create a NewCarpet by "expanding" every row of the old carpet. */
 recarpet([], []).
 recarpet([Row|Carpet], NewCarpet) :-
 	expand_row(Row, NewRows),
 	append(NewRows, PrevCarpet, NewCarpet),
 	recarpet(Carpet, PrevCarpet).
 	
+/* Apply rewrite to every char of Row and return a list of lists. */
 expand_row([], []).
 expand_row([X|Row], NewRows) :-
 	rewrite(X, RL),
 	expand_row(Row, SoFarRows),
 	add_to(RL, SoFarRows, NewRows).
 
+/* Used by expand_row/2 to gradually create the list of lists to be returned. */
 add_to([], [], []).
 add_to(RL, [], RL).
 add_to([R|RL], [SoFarR|SoFarRows], [NewR|NewRows]) :-
 	append(R, SoFarR, NewR), 
 	add_to(RL, SoFarRows, NewRows).
-	
-carpet_print([]).
+
+/* Print every	row of carpet. */
+carpet_print([]).	
 carpet_print([Row|Carpet]) :-
 	row_print(Row),
-	print('\n'),
+	write('\n'),
 	carpet_print(Carpet).
 	
+/* Print every char of row. */
 row_print([]).
 row_print([X|R]) :-
-	print(X),
+	write(X),
 	row_print(R).
--- frogs.pl
+++ frogs.pl
@@ -1,3 +1,9 @@
+/* Create a list depiction of the initial state of the problem 
+ * (e.g. ['g', 'g', 'g', 'g', ' ', 'b', 'b', 'b'] for N == 4 and M == 3)
+ * and return the list of moves that, if applied to it, would yield 
+ * its final state (['b', 'b', 'b', ' ', 'g', 'g', 'g', 'g'] for that same
+ * example) using solve/2 */
+
 frogs(0, 0, []).
 frogs(N, M, Solution) :-
 	create_list('g', N, GList),
@@ -8,11 +14,15 @@ frogs(N, M, Solution) :-
 	append(NewBList, GList, FinalState),
 	solve(InitialState, FinalState, [InitialState], Solution).
 	
+/* Creates a list consisting of exactly N Xs. */
 create_list(_, 0, []).
-create_list(X, N, L) :-
+create_list(X, N, L) :-	
 	length(L, N),
 	maplist(=(X), L).
-	
+
+/* Returns a list of moves from a CurrentState to the FinalState
+ * using move/3, without making moves that would result in a state
+ * that we've already been before. */
 solve(FinalState, FinalState, _, []).
 solve(CurrentState, FinalState, States, [Move|Solution]) :-
 	CurrentState \= FinalState,
@@ -20,6 +30,7 @@ solve(CurrentState, FinalState, States, [Move|Solution]) :-
 	not(member(NextState, States)),
 	solve(NextState, FinalState, [NextState|States], Solution).
 	
+/* All possible moves are explicitly described here: */
 move(['g', ' ', 'b'|Rest], [' ', 'g', 'b'|Rest], "g1").
 move(['b', 'g', ' '|Rest], ['b', ' ', 'g'|Rest], "g1").
 move(['g', 'b', ' '|Rest], [' ', 'b', 'g'|Rest], "g2").
