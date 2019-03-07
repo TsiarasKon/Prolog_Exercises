f32fa54dd8e6d72027da05935382bd62e83d490c
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-03-07 16:14:29 +0200

    A1: using grid

--- LP19/LP_Ex1.pl
+++ LP19/LP_Ex1.pl
@@ -1,12 +1,22 @@
 % disnake([a,b,c,d,e,f,g], [_,_,_,_,_,_,_,_,_,_,_], [_,_,_,_,_,_]).
+% disnake([0,1,2,3,4,5,6,7,8,9], [_,_,_,_], [_,_,_]).
  
-/*
-writelist([]).
-writelist([X | List]) :-
-	write(X),
-	writelist(List).
 
+write_list([]).
+write_list([X | List]) :-
+	write(X),
+	write_list(List).
 	
+write_grid([]).
+write_grid([R1 | RRest]) :-
+	write_list(R1),
+	write('\n'),
+	write_grid(RRest).
+	
+my_prepend(L1, L2, L3) :-
+	append(L2, L1, L3).
+
+/*	
 disnake(Pattern, _, [_ | List]) :- 
 	writelist(Pattern),
 	write('\n'),
@@ -25,7 +35,7 @@ disnakebool(Pattern, _, [_ | List], false) :-
 	write('\n'),
 	disnakebool(Pattern, _, List, true).
 	
-*/
+
 
 disnake(Pattern, Cols, Rows) :-
 	disnakehelper(Pattern, Cols, Rows, Pattern, Cols).
@@ -40,3 +50,27 @@ disnakehelper(Pattern, Cols, Rows, [], C) :-
 disnakehelper(Pattern, Cols, [_ | RRest], P, []) :-
 	write('\n'),
 	disnakehelper(Pattern, Cols, RRest, P, Cols).
+
+*/
+
+disnake(Pattern, Cols, Rows) :-
+	disnakehelper(Pattern, Cols, Rows, Pattern, Cols, [[]]).
+	
+disnakehelper(Pattern, Cols, Rows, [P1 | PRest], [_ | CRest], [G1 | GRest]) :-
+	append(G1, [P1], NewG1),
+	disnakehelper(Pattern, Cols, Rows, PRest, CRest, [NewG1 | GRest]).
+	
+disnakehelper(Pattern, Cols, Rows, [], C, Grid) :-
+	disnakehelper(Pattern, Cols, Rows, Pattern, C, Grid).
+	
+disnakehelper(Pattern, Cols, [_ | RRest], P, [], Grid) :-
+	my_prepend(Grid, [[]], NewGrid),
+	disnakehelper(Pattern, Cols, RRest, P, Cols, NewGrid).
+	
+disnakehelper(_, _, [], _, [], Grid) :-
+	reverse(Grid, GridRev),
+	write_grid(GridRev).
+	
+	
+	
+	
