e4bfdd8b756ccd0b6a3fbad4b30b7a758d7be8fc
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-03-17 19:27:16 +0200

    A2: effectively working
    Returns N! solutions however, instead of N distinct ones

--- LP19/assignment.pl
+++ LP19/assignment.pl
@@ -1,5 +1,5 @@
 % Delet these:
-
+/*
 activity(a01, act(0,3)).
 activity(a02, act(0,4)).
 activity(a03, act(1,5)).
@@ -15,12 +15,12 @@ activity(a12, act(16,18)).
 activity(a13, act(17,19)).
 activity(a14, act(18,20)).
 activity(a15, act(19,20)).
+*/
 
-/*
 activity(a1, act(0,3)).
 activity(a2, act(4,6)).
 activity(a3, act(1,2)).
-*/
+
 
 assignment(NP, ST, ASP, ASA) :-
 	findall(A, activity(A, act(_, _)), AL),
@@ -38,14 +38,15 @@ assignmentHelper(_, ASP, [], ASP, []).
 assignmentHelper(ST, ASP, [A - N | ASARest], ASPBefore, [A | ALRest]) :-
 	assignActivity(A, ST, ASPBefore, ASPAfter, N),
 	assignmentHelper(ST, ASP, ASARest, ASPAfter, ALRest).
-
+	
+/*
 assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
 	append(ASP1, [N - [] - 0 | ASP2], ASPBefore),
 	activity(A, act(AStart, AEnd)),
 	TSum1 is AEnd - AStart,
 	TSum1 =< ST,
 	append(ASP1, [N - [A] - TSum1 | ASP2], ASPAfter).
-	
+
 assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
 	append(ASP1, [N - [LastA | NL] - TSum | ASP2], ASPBefore),
 	activity(A, act(AStart, AEnd)),
@@ -55,4 +56,22 @@ assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
 	TSum1 is TSum + AEnd - AStart,
 	TSum1 =< ST,
 	append(ASP1, [N - [A, LastA | NL] - TSum1 | ASP2], ASPAfter).
+*/
 
+assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
+	append(ASP1, [N - NL - TSum | ASP2], ASPBefore),
+	activity(A, act(AStart, AEnd)),
+	canBeAssigned(AStart, AEnd, N, ASPBefore),
+	TSum1 is TSum + AEnd - AStart,
+	TSum1 =< ST,
+	append(ASP1, [N - [A | NL] - TSum1 | ASP2], ASPAfter).
+	
+canBeAssigned(AStart, AEnd, N, ASP) :-
+	member(N - NL - _, ASP),
+	listTimeCheck(NL, AStart, AEnd).
+	
+listTimeCheck([], _, _).
+listTimeCheck([PastA | NL], AStart, AEnd) :-
+	activity(PastA, act(PastAStart, PastAEnd)),
+	(AEnd < PastAStart; AStart > PastAEnd),
+	listTimeCheck(NL, AStart, AEnd).
