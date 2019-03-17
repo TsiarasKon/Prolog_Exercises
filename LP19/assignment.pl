2daff869cafc1ababe7c0bb6a06db2c2755139fe
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-03-17 18:30:09 +0200

    A2: single case working

--- LP19/assignment.pl
+++ LP19/assignment.pl
@@ -1,5 +1,4 @@
 % Delet these:
-% activity(a01, act(0,3)).
 /*
 activity(a02, act(0,4)).
 activity(a03, act(1,5)).
@@ -16,18 +15,10 @@ activity(a13, act(17,19)).
 activity(a14, act(18,20)).
 activity(a15, act(19,20)).
 */
-/*
+
 activity(a1, act(0,3)).
 activity(a2, act(4,6)).
-activity(a3, act(1,2)).
-
-% Useless
-
-createStartingASP([], [], _).
-createStartingASP([A | LRest], [N - [] - 0 | ASARest], N) :-
-	N1 is N + 1,
-	createStartingASP(LRest, ASARest, N1).
-*/
+%activity(a3, act(1,2)).
 
 assignment(NP, ST, ASP, ASA) :-
 	findall(A, activity(A, act(_, _)), AL),
@@ -41,10 +32,10 @@ createStartingASP(N, [N - [] - 0 | ASPRest]) :-
 	N1 is N - 1,
 	createStartingASP(N1, ASPRest).
 	
-assignmentHelper(_, _, _, _, []).
-assignmentHelper(ST, ASP, [A - N | ASARest], ASPBefore, [A | ALRest]) :-
+assignmentHelper(_, _, [], _, []).
+assignmentHelper(ST, ASPAfter, [A - N | ASARest], ASPBefore, [A | ALRest]) :-
 	assignActivity(A, ST, ASPBefore, ASPAfter, N),
-	assignmentHelper(ST, ASP, ASARest, ASPAfter, ALRest).
+	assignmentHelper(ST, ASPAfter, ASARest, ASPAfter, ALRest).
 
 assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
 	append(ASP1, [N - [] - 0 | ASP2], ASPBefore),
@@ -53,10 +44,13 @@ assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
 	TSum1 =< ST,
 	append(ASP1, [N - [A] - TSum1 | ASP2], ASPAfter).
 	
-/*
-assignActivity(A, ST, ASABefore, ASAAfter, N) :-
-	append(ASA1, [N - NL - TSum | ASA2], ASABefore),
+assignActivity(A, ST, ASPBefore, ASPAfter, N) :-
+	append(ASP1, [N - [LastA | NL] - TSum | ASP2], ASPBefore),
 	activity(A, act(AStart, AEnd)),
-	
-	append(ASA1, [N - [A | NL] - TSum1 | ASA2], ASAAfter).
-*/
+	activity(LastA, act(_, LastAEnd)),
+	MinStartT is LastAEnd + 1,
+	AStart >= MinStartT,
+	TSum1 is TSum + AEnd - AStart,
+	TSum1 =< ST,
+	append(ASP1, [N - [A, LastA | NL] - TSum1 | ASP2], ASPAfter).
+
