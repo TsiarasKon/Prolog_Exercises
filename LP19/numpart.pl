ce5b36b6af15eb78d8fbc86e6268314d2ebb6d30
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-04-16 21:25:48 +0300

    B3: checking square lists before search() ...which is actually NOT faster

--- LP19/numpart.pl
+++ LP19/numpart.pl
@@ -7,21 +7,30 @@ numpart(N, L1, L2) :-
     HalfN is integer(N / 2),
     length(L1, HalfN),
     length(L2, HalfN),
+    squareList(L1, SqL1),
+    squareList(L2, SqL2),
     [L1, L2] #:: 1..N,
-    sumlist(L1, SumL),
-    sumlist(L2, SumL),
+    firstList(L1, L2),
     append(L1, L2, L),
     fd_global:alldifferent(L),
-    firstList(L1, L2),
     ordered(<, L1),
     ordered(<, L2),
-    search([L1, L2], 0, first_fail, indomain, complete, []),
-    powListSum(L1, 2, SquareSum),
-    powListSum(L2, 2, SquareSum).
+    sumlist(L1, SumL),
+    sumlist(L2, SumL),
+    sumlist(SqL1, SqSumL),
+    sumlist(SqL2, SqSumL),
+    search([L1, L2], 0, first_fail, indomain_reverse_min, complete, []).
+    % powListSum(L1, 2, SquareSum),
+    % powListSum(L2, 2, SquareSum).
 
 % used to avoid duplicate results
 firstList([X | _], [Y | _]) :-
     X #< Y.
+
+squareList([], []).
+squareList([X | L], [SqX | SqL]) :-
+    SqX #= X * X,
+    squareList(L, SqL).
 
 powListSum([], _, 0).
 powListSum([X | L], Pow, PowSum) :-
