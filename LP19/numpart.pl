4bc2b3194f5856776e23d3421ec0a59261e76729
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-04-16 21:34:42 +0300

    B3: currently fastest version

--- LP19/numpart.pl
+++ LP19/numpart.pl
@@ -7,8 +7,6 @@ numpart(N, L1, L2) :-
     HalfN is integer(N / 2),
     length(L1, HalfN),
     length(L2, HalfN),
-    squareList(L1, SqL1),
-    squareList(L2, SqL2),
     [L1, L2] #:: 1..N,
     firstList(L1, L2),
     append(L1, L2, L),
@@ -17,20 +15,13 @@ numpart(N, L1, L2) :-
     ordered(<, L2),
     sumlist(L1, SumL),
     sumlist(L2, SumL),
-    sumlist(SqL1, SqSumL),
-    sumlist(SqL2, SqSumL),
-    search([L1, L2], 0, first_fail, indomain_reverse_min, complete, []).
-    % powListSum(L1, 2, SquareSum),
-    % powListSum(L2, 2, SquareSum).
+    search([L1, L2], 0, first_fail, indomain_reverse_min, complete, []),
+    powListSum(L1, 2, SquareSum),
+    powListSum(L2, 2, SquareSum).
 
 % used to avoid duplicate results
 firstList([X | _], [Y | _]) :-
     X #< Y.
-
-squareList([], []).
-squareList([X | L], [SqX | SqL]) :-
-    SqX #= X * X,
-    squareList(L, SqL).
 
 powListSum([], _, 0).
 powListSum([X | L], Pow, PowSum) :-
