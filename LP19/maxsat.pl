0f80d80436328646abc66e40e44b1177595cd60d
TsiarasKon <tsiaraskon@gmail.com>
2019-05-06 11:32:24 +0300

    B4: more successful attempts

--- LP19/maxsat.pl
+++ LP19/maxsat.pl
@@ -3,33 +3,31 @@
 :- lib(branch_and_bound).
 :- lib(listut).
 
-
-
-evalClause([], _, 0).
-evalClause([L | _], S, 1) :-
-    L > 0,
-    element(L, S, Elem),
-    Elem #= 1.
-evalClause([L | _], S, 1) :-
-    L < 0,
-    Lindex is -L,
-    element(Lindex, S, Elem),
-    Elem #= 0.
-evalClause([_ | Rest], S, Value) :-
-    evalClause(Rest, S, Value).
-
 % evalClause([], _, 0).
-% evalClause([L | _], S, Elem) :-
+% evalClause([L | _], S, 1) :-
 %     L > 0,
-%     nth1(L, S, Elem), !.
-% evalClause([L | _], S, Elem1) :-
+%     element(L, S, Elem),
+%     Elem #= 1.
+% evalClause([L | _], S, 1) :-
 %     L < 0,
 %     Lindex is -L,
-%     nth1(Lindex, S, Elem),
-%     Elem1 #= Elem + 1, !.
+%     element(Lindex, S, Elem),
+%     Elem #= 0.
 % evalClause([_ | Rest], S, Value) :-
 %     evalClause(Rest, S, Value).
 
+evalClause([], _, 0).
+evalClause([L | _], S, Elem) :-
+    L > 0,
+    nth1(L, S, Elem).
+evalClause([L | _], S, Elem1) :-
+    L < 0,
+    Lindex is -L,
+    nth1(Lindex, S, Elem),
+    Elem1 #:: 0..1,
+    Elem1 #\= Elem.
+evalClause([_ | Rest], S, Value) :-
+    evalClause(Rest, S, Value).
 
 evalCNF([], _, 0).
 evalCNF([C | Rest], S, M1) :-
@@ -39,19 +37,13 @@ evalCNF([C | Rest], S, M1) :-
 
 maxsat(NV, NC, D, F, S, M) :-
     % create_formula(NV, NC, D, F),
-    % F = [[1], [-2], [-1, -2], []],
-    F = [[-4], [-4, -5], [3, -4], [-1], [-2, 5], [], [1, 4],
-    [3, 5], [], [4], [1, 2, -4, -5], [-3], [-4], [-3], [],
-    [1], [-5], [1, -5], [], [1, -4]],
+    F = [[1], [-2], [-1, -2], []],
+    % F = [[-4], [-4, -5], [3, -4], [-1], [-2, 5], [], [1, 4],
+    % [3, 5], [], [4], [1, 2, -4, -5], [-3], [-4], [-3], [],
+    % [1], [-5], [1, -5], [], [1, -4]],
     % F = [[1,-2,4],[-1,2],[-1,-2,3],[-2,-4],[2,-3],[1,3],[-3,4]],
     length(S, NV),
     S #:: 0..1,
-    write(S),
-    nl,
-    isFirst(S, XX),
-    write(XX),
-    nl,
-    % occurrences(1, S, M),
     evalCNF(F, S, M),
     NegM #= -M,
     bb_min(labeling(S), NegM, _).
@@ -60,6 +52,3 @@ maxsat(NV, NC, D, F, S, M) :-
 % F = [[-4], [-4, -5], [3, -4], [-1], [-2, 5], [], [1, 4],
 %  [3, 5], [], [4], [1, 2, -4, -5], [-3], [-4], [-3], [],
 %  [1], [-5], [1, -5], [], [1, -4]], evalCNF(F, [1, 1, 0, 0, 1], M).
-
-isFirst([Y | _], X) :-
-    X #= Y.
